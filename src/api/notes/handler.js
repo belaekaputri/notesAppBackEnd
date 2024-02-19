e8fb38ac0cf1fbab04ad7b14e61def88272de0fe
belaekaputri <belaekaputri28@gmail.com>
2024-02-19 12:56:11 +0700

    data validation

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -1,9 +1,9 @@
 // fungsi handler untuk menangani permintaan dari client yg data lalu member respon.
 
 class NotesHandler {
-  constructor(service) {
+  constructor(service, validator) {
     this._service = service;
-
+    this._validator = validator;
     this.postNoteHandler = this.postNoteHandler.bind(this);
     this.getNotesHandler = this.getNotesHandler.bind(this);
     this.getNoteByIdHandler = this.getNoteByIdHandler.bind(this);
@@ -13,6 +13,7 @@ class NotesHandler {
 
   postNoteHandler(request, h) {
     try {
+      this._validator.validateNotePayload(request.payload);
       const { title = 'untitled', body, tags } = request.payload;
       const noteId = this._service.addNote({ title, body, tags });
 
@@ -67,6 +68,7 @@ class NotesHandler {
 
   putNoteByIdHandler(request, h) {
     try {
+      this._validator.validateNotePayload(request.payload);
       const { id } = request.params;
 
       this._service.editNoteById(id, request.payload);
